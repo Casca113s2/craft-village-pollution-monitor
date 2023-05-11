0fdf25af2939a5b137900404d11bceb9a896e315
duchuyltt122 <duchuyltt122@gmail.com>
2023-05-11 11:49:46 +0700

    Fix question 66 and 67

--- API_AI/app.py
+++ API_AI/app.py
@@ -48,6 +48,20 @@ def convert_json_to_dataframe(data):
                         new_array.append(1)
                     else:
                         new_array.append(2)
+                elif (id == 66 or id == 67):
+                    x = 0
+                    if (answer[0]["count"] == 1):
+                        if(answer[0]["value"]=="<20%"):
+                            x = 10
+                        elif (answer[0]["value"] == "20% - 40%"):
+                            x = 30
+                        elif(answer[0]["value"]=="40%- 60%"):
+                            x = 50
+                        elif (answer[0]["value"] == "60% - 80%"):
+                            x = 70
+                        elif (answer[0]["value"] == "> 80%"):
+                            x = 90
+                    new_array.append(x)
                 else:
                     if (answer[0]["count"] == 1):
                         new_array.append(float(answer[0]["value"]))
